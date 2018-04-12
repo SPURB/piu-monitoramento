d830578d06e006a0dece20fbc60b786132308d1b
davimh <hosogiri.davi@gmail.com>
2018-04-12 16:35:18 -0300

    CSS e js sumario e comeco ficha

--- dev/components/ficha.js
+++ dev/components/ficha.js
@@ -7,8 +7,57 @@ let ficha = {
 	},
 	template: `
 	<div id="ficha">
-		<h3>Ficha</h3>
-		{{ data }}
+
+		<div class="colId">
+			<div class="indicador">indica</div>
+			<div class="id">
+				<ul>
+					<li>
+						Natureza da proposta
+						-natureza-
+					</li>
+					<li>
+						Proponente
+						-proponente-
+					</li>
+					<li>
+						Origem
+						-origem-
+					</li>
+					<li>
+						Registro administrativo
+						-registro-
+					</li>
+					<li>Documentos</li>
+				</ul>
+
+			</div>
+		</div>
+
+		<div class="aspectos">
+			<h4>Aspectos técnico-urbanísticos</h4>
+			<p>Escopo</p>
+			<p>Descrição</p>
+		</div>
+
+		<div class="tramitacao">
+			<h4>Tramitação prevista</h4>
+			<ol>
+				<li><span>Em proposição dos elementos prévios</span><i class="material-icons">check_box</i></li>
+				<li><span>1ª Consulta Pública (Pré-PIU)</span><i class="material-icons">indeterminate_check_box</i></li>
+				<li><span>Em avaliação após 1ª Consulta</span><i class="material-icons">check_box_outline_blank</i></li>
+				<li class="anterior"><span>Em avaliação SMUL</span></li>
+				<li><span>PIU em elaboração</span></li>
+				<li><span>Discussão pública</span></li>
+				<li><span>Consolidação PIU</span></li>
+				<li><span>Encaminhamento Jurídico PMSP</span></li>
+				<li><span>Consolidado em Decreto</span></li>
+				<li><span>Em tratativa na CMSP</span></li>
+				<li><span>Consolidado em Lei</span></li>
+				<li><span>Aprovado - em implantação</span></li>
+			</ol>
+		</div>
+
 	</div>
 	`
 }
--- dev/components/sumario.js
+++ dev/components/sumario.js
@@ -11,22 +11,205 @@ let sumario = {
 	template: `
 	<div id="sumario">
 
-		<div v-for="projeto in data">
+		<div id="proposicao">
+			<div class="etapa">
+				<div>Em proposição</div>
+				<div>PIUs públicos</div>
+				<div>PIUs privados</div>
+			</div>
 
-			<div v-if="projeto.subetapa<=3">
+			<div class="subetapa">
+				<div class="titulo">01 <span>Em proposição dos elementos prévios</span></div>
 				<div>
-					<div>Em proposição</div>
-					<div>PIUs públicos</div>
-					<div>PIUs privados</div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==1">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
 				</div>
-
-				<div v-if="projeto.subetapa==1">
-					<div v-for="projeto.natureza==publico">{{ projeto.nome }}</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==1">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
 				</div>
 			</div>
 
+			<div class="subetapa">
+				<div class="titulo">02 <span>1ª Consulta Pública</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==2">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==2">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">03 <span>Em avaliação após 1ª Consulta</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==3">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==3">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
 		</div>
 
+		<div id="avaliacao">
+			<div class="etapa">
+				<div>Em avaliação SMUL</div><div></div><div></div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">04 <span>Em avaliação SMUL</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==4">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==4">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="elaboracao">
+			<div class="etapa">
+				<div>Em elaboração</div><div></div><div></div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">05 <span>PIU em elaboração</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==5">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==5">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">06 <span>Discussão pública</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==6">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==6">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">07 <span>Consolidação PIU</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==7">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==7">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">08 <span>Encaminhamento jurídico PMSP</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==8">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==8">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="tramitacao">
+			<div class="etapa">
+				<div>Tramitação jurídica</div><div></div><div></div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">09 <span>Consolidado em Decreto</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==9">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==9">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">10 <span>Em tratativa na CMSP</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==10">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==10">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">11 <span>Consolidado em Lei</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==11">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==11">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="implantacao">
+			<div class="etapa">
+				<div>Em implantação</div><div></div><div></div>
+			</div>
+
+			<div class="subetapa">
+				<div class="titulo">12 <span>Aprovado - em implantação</span></div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Pública' && projeto.subetapa==12">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+				<div>
+					<div v-for="projeto in data" v-if="projeto.natureza=='Privado' && projeto.subetapa==12">
+						<a href="#">{{ projeto.nome }}</a>
+					</div>
+				</div>
+			</div>
+		</div>
 
 	</div>
 	`
