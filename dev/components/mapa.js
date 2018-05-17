8022568e7bdbd5a20481552f80bdf682a18bc2cc
davimh <hosogiri.davi@gmail.com>
2018-05-17 17:40:48 -0300

    Integrando mapa com sumario

--- dev/components/mapa.js
+++ dev/components/mapa.js
@@ -1,22 +1,97 @@
+function tst(){
+	const centroInicial = [-5190080.00000,-2708530.34945];
+
+	var base_map = new ol.layer.Tile({ source: new ol.source.OSM() });
+
+	var view = new ol.View({
+		projection: ol.proj.get('EPSG:3857'),
+		center: ol.proj.fromLonLat([-51.9,-27]),
+		zoom: 0,
+		minZoom: 4,
+		maxZoom: 19
+	});
+
+	var map = new ol.Map({
+		target: 'map',
+		layers: [
+			base_map	
+			// msp,
+			// arco_jurbatuba
+		],
+		view: view,
+		loadTilesWhileAnimating: true,
+	});
+}
+function teste(x,y){
+	const centroInicial = [-5190080.00000,-2708530.34945];
+
+	var base_map = new ol.layer.Tile({ source: new ol.source.OSM() });
+
+	var view = new ol.View({
+		projection: ol.proj.get('EPSG:3857'),
+		center: ol.proj.fromLonLat([-51.9,-27]),
+		zoom: 0,
+		minZoom: 4,
+		maxZoom: 19
+	});
+
+	var map = new ol.Map({
+		target: 'map',
+		layers: [
+			base_map	
+			// msp,
+			// arco_jurbatuba
+		],
+		view: view,
+		loadTilesWhileAnimating: true,
+	});
+	console.log(x,y);
+	view.animate({
+		center: ol.proj.fromLonLat([x,y]),
+		duration: 5000
+	});
+}
+
 let mapa = {
 	name:'mapa',
 	data (){
 		return {
 			data: monitoramento,
 			projeto: '',
+			centro_x: '', centro_y: '',
 		}
-		var centro = ol.proj.fromLonLat([-50,-50]);
 	},
 	props: ['clicked-id'],
 	methods: {
-		encontraProjeto(newClickedId) {
-			this.data.map(function(index) {
-				if (index.id == newClickedId) {
-					this.projeto = index;
+	},
+	mounted(){
+		// tst()
+		// var msp = new ol.layer.Vector({
+		// 	source: new ol.source.Vector({
+		// 		url: dist_folder + 'kml/limite_msp.kml',//dist_folder definido em index.html ou page-piu-monitoramento.php
+		// 		format: new ol.format.KML()
+		// 	})
+		// });
+
+		// var arco_jurbatuba = new ol.layer.Vector({
+		// 	source: new ol.source.Vector({
+		// 		url: dist_folder + 'kml/piu-arco-jurubatuba_2018-04.kml',
+		// 		format: new ol.format.KML()
+		// 	})
+		// });
+	},
+	watch:{
+		clickedId(newprop,oldprop){
+			const app = this;
+			app.data.map(function(index) {
+				if (index.ID_rev == newprop) {
+					app.projeto = index;
+					app.centro_x = app.projeto.mapateste_x;
+					app.centro_y = app.projeto.mapateste_y;
+					teste(app.centro_x, app.centro_y);
 				};
 			});
-		},
-
+		}
 	},
 	template: `
 	<div id="mapa">
