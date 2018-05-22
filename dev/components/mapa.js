1994792d1f063f833dae38ede37703ac9607519f
yubathom <yubathom@gmail.com>
2018-05-22 19:08:18 -0300

    Adiciona KML's, cores por etapas implementado

--- dev/components/mapa.js
+++ dev/components/mapa.js
@@ -14,22 +14,7 @@ let mapa = {
 	data (){
 		return {
 			data: monitoramento,
-			projeto: undefined,
-			layers: {
-				base_map: 
-					new ol.layer.Tile({ 
-						source: new ol.source.OSM() 
-					})
-				,
-				limites_msp: 
-					new ol.layer.Vector({ 
-						source: new ol.source.Vector({
-							url: dist_folder + 'kml/BASE_Limite_MSP.kml',
-							format: new ol.format.KML()
-						}) 
-					})
-				,
-			}
+			projeto: undefined
 		}
 	},
 	props: ['clicked-id'],
@@ -38,8 +23,8 @@ let mapa = {
 			let parseKml = [];
 			kmls.map(function(str) { // dev/data/kmls.js
 				let outputid
-				let baseNumber = str.substring(0,2) // primeiros dois strings
-				let numericStr = parseInt(baseNumber,10) // converte string para número ou retorna NaN
+				let baseNumber = str.substring(0,2) // primeiros dois strings -> 1_ ou 10 
+				let numericStr = parseInt(baseNumber,10) // converte strings para número ou retorna NaN
 				Number.isNaN(numericStr) ? // se string não é numérica  
 					outputid = str.substring(0, 4) : // retorna 4 caracteres -> 'BASE'
 					outputid = numericStr.toString() // Converte valor numérico em string 
@@ -49,15 +34,87 @@ let mapa = {
 				})
 			})
 			return parseKml;
+		},
+		kmlLayers(){
+			let output = [
+				new ol.layer.Tile({ 
+					source: new ol.source.OSM()
+				}) 
+			]
+
+			function defineStyle(id){
+				let id_projeto = undefined
+				let etapa = undefined
+				let outputColor = {
+					stroke: 'rgba()',
+					fill: 'rgba(200,200,200, 0.5)'
+				}
+
+				monitoramento.map(function(index) {
+					if(index.ID_rev == id) {
+						id_projeto = id;
+						etapa = index.etapas_NUM
+					}
+				}) 
+
+				// console.log('id: '+ id, 'etapa: ' + parseInt(etapa, 10))
+				let etapaNumber = parseInt(etapa, 10)
+
+				// #ffccb3 	-> Em proposiçao
+				// #f50 	-> Em andamento
+				// #802b00 	-> Implantação
+				// #bdbdbd 	-> Suspenso
+				if (id == 'BASE'){
+					outputColor.stroke = 'rgba(50,50,50, 1)'
+					outputColor.fill = 'rgba(255,255,255, 0.75)'
+				}
+				else if(etapaNumber < 3){
+					outputColor.fill = '#ffccb3'
+				}
+				else if(3 < etapaNumber < 6){
+					outputColor.fill = '#f50'
+				}
+				else if(6 < etapaNumber < 9){
+					outputColor.fill = '#802b00'
+				}
+				else if(9 < etapaNumber){
+					outputColor.fill = '#bdbdbd'
+				}
+
+				let style = new ol.style.Style({
+					stroke: new ol.style.Stroke({
+						color: outputColor.stroke,
+						width: 1
+					}),
+						fill: new ol.style.Fill({
+						color: outputColor.fill
+					})
+				});
+				return style
+			}
+
+			this.kmls.map(function(object) {
+				let layer =  new ol.layer.Vector({ 
+					style: defineStyle(object.id),
+					source: new ol.source.Vector({
+						url: dist_folder + 'kml/' + object.fileName,
+						format: new ol.format.KML({
+							extractStyles: false,
+						}),
+					}),
+					updateWhileAnimating: true,
+					renderBuffer:100,
+					renderMode: 'image'
+				})
+				output.splice(1,0,layer)
+			})
+			return output
 		}
 	},
 	mounted(){
-		let map = new ol.Map({ // Cria mapa
+		let map = new ol.Map({
 			target: 'map',
-			layers: [
-				this.layers.base_map,
-				this.layers.limites_msp
-			],
+			layers: this.kmlLayers,
 			loadTilesWhileAnimating: true,
 			view: view
 		});
@@ -80,7 +137,7 @@ let mapa = {
 				zoom: 14,
 				duration: 1500
 			});
-		},
+		}
 	}, 
 	template: `
 	<div id="mapa">
