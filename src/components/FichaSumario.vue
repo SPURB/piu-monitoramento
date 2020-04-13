e18394ce27aa18f9694297dabf8dc3ec23f795b9
yubathom <yubathom@gmail.com>
2020-04-13 14:09:03 -0300

    refactor: aplica BEM em FichaSumario

--- src/components/FichaSumario.vue
+++ src/components/FichaSumario.vue
@@ -1,23 +1,23 @@
 <template>
-	<div class="colId">
+	<div class="ficha-sumario">
 		<div class="indicador" :class="etapaClass" title="Etapa atual da tramitação do PIU">{{ etapaTag }}</div>
 			<div class="indicador__group">
 				<template v-if="isNotEmpty(this.projeto.id_iniciativa_da_proposta)">
 					Natureza da proposta
-					<div>{{projeto.id_iniciativa_da_proposta}}</div>
+					<div class="group__item">{{projeto.id_iniciativa_da_proposta}}</div>
 				</template>
 				<template v-if="isNotEmpty(projeto.id_proponente)">
 					Proponente
-					<div>{{projeto.id_proponente}}</div>
+					<div class="group__item">{{projeto.id_proponente}}</div>
 				</template>
 				<template v-if="isNotEmpty(projeto.id_origem)">
 					Origem
-					<div>{{projeto.id_origem}}</div>
+					<div class="group__item">{{projeto.id_origem}}</div>
 				</template>
 				<template v-if="isNotEmpty(projeto.id_registro_administrativo)">
 					Registro administrativo
-					<div v-for="(link, index) in filteredLinks" :key="index">
-						<a :href="link.arquivo" :title="link.nome_publico_do_arquivo" target="_blank">
+					<div class="group__item" v-for="(link, index) in filteredLinks" :key="index">
+						<a :class="{'item__last': index === filteredLinks.length - 1}" :href="link.arquivo" :title="link.nome_publico_do_arquivo" target="_blank">
 							{{ link.nome_publico_do_arquivo }} <i class="material-icons">launch</i>
 						</a>
 					</div>
@@ -82,7 +82,7 @@ export default {
 <style lang="scss" scoped>
 @import "../scss/variables";
 
-.colId {
+.ficha-sumario {
 	width: 20%;
 	font-size: 1em;
 	line-height: 1.75em;
@@ -91,52 +91,60 @@ export default {
 	-webkit-hyphens: auto;
 	-moz-hyphens: auto;
 	hyphens: auto;
-	.indicador__group {
-		border-radius: 2px;
-		border: 1px solid #BDBDBD;
-		padding: 4px 8px;
-		margin-top: 16px;
-		margin-bottom: 16px;
-		div { font-weight: 700; margin-bottom: 1em; };
-		div:last-child { margin-bottom: 0; };
-		a {
-			font-weight: 700;
-			display: block;
+}
 
-			i {
-				font-size: 16px;
-				line-height: 13px;
-				vertical-align: -3px;
-			};
+.indicador__group {
+	border-radius: 2px;
+	border: 1px solid #BDBDBD;
+	padding: 4px 8px;
+	margin-top: 16px;
+	margin-bottom: 16px;
+}
 
-			&:not([href]) {
-				color: initial;
+.group__item { 
+	font-weight: 700;
+	margin-bottom: 0;
+	a {
+		margin-bottom: 1rem;
+		font-weight: 700;
+		display: block;
 
-				&:hover { text-decoration: none; cursor: default;};
-
-				i { display: none; };
-			};
+		i {
+			font-size: 16px;
+			line-height: 13px;
+			vertical-align: -3px;
 		};
-	};
-	a.link_pag_completa {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		hyphens: none;
-		padding: .4em;
-		border-radius: 2px;
-		text-align: center;
-		background-color: $vermelho-gestao;
-		color: #FFFFFF;
-		text-decoration: none;
-		border: 1px solid $vermelho-gestao;
-		transition: all 200ms;
-		max-height: 40px;
-		i { font-size: 16px; margin-left: 4px; };
-		&:hover {
-			background-color: transparent;
-			color: $vermelho-gestao;
+
+		&:not([href]) {
+			color: initial;
+			&:hover { text-decoration: none; cursor: default;};
+			i { display: none; };
 		}
+		&.item__last {
+			margin-bottom: 0
+		}
+	}
+}
+
+
+.link_pag_completa {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	hyphens: none;
+	padding: .4em;
+	border-radius: 2px;
+	text-align: center;
+	background-color: $vermelho-gestao;
+	color: #FFFFFF;
+	text-decoration: none;
+	border: 1px solid $vermelho-gestao;
+	transition: all 200ms;
+	max-height: 40px;
+	i { font-size: 16px; margin-left: 4px; };
+	&:hover {
+		background-color: transparent;
+		color: $vermelho-gestao;
 	}
 }
 </style>
